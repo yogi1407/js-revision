f642528fd1250f640c965c137f8ba12ae039a993
yogi1407 <73849556+yogi1407@users.noreply.github.com>
2024-12-24 21:17:08 +0000

    changess

--- 01-basics/01-variables.js
+++ 01-basics/01-variables.js
@@ -1,9 +1,9 @@
- const accId = 123456
- let accountEmail = "yogi@google.com"
- var accountPass = "1234"
- accountCity = "jaipur"
- let accountState;
- console.log(accId)
- 
+const accId = 123456
+let accountEmail = "yogi@google.com"
+var accountPass = "1234"
+accountCity = "jaipur"
+let accountState = null;
+console.log(accId)
+
 //  prefer not to use var because of the issue in the block scope and functional scope
-console.table([accId , accountEmail , accountPass ,accountCity , accountState])
+console.table([accId, accountEmail, accountPass, accountCity, accountState])
---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //this is to treat the whole js code in newer version
+//alert( 3 + 3 )//this will not work because it is node js and not browser i'll work only inn browser with this syntax
+//code should be readable as much as possible
+// remember tc39.es and mdn this is for datatypes
+let name = " yogi";
+let age = 24;
+let isloggedin = false;
+let state;
+
+//number =>2 to the power 52
+//bigInt => it is used for very big numbers
+// string => ""
+// boolean => true/false
+//null => it is a standalone value
+//undefined =>
+// symbol => it is used for uniqueness
+//object =>
+ console.log(typeof undefined);// undefined
+console.log(typeof null);//object
+ 
