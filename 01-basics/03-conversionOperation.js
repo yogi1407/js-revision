42e04f111b0ba7fdb001dd24a23ca663fb805e97
yogi1407 <73849556+yogi1407@users.noreply.github.com>
2024-12-25 20:26:42 +0000

    comperision changes

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,81 @@
+//let score = "333"
+//let score = "333aabbcc"
+let score = "333aabbcc"
+
+
+// console.log(typeof score);
+// console.log(typeof (score));//both are same
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+// 33 is converted into number itself 33
+// true convertes to 1 , false converts to 0
+//33aabbcc converts to NaN(not a number)
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 = true , 0 = false
+// " " = false
+// "yogi" = true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//********************************************* OPERATIONS *********************************************************
+
+
+let value = 3
+let negvalue = -value
+
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2); multiplication
+// console.log(2**2); for making power
+// console.log(2/2); for devision
+// console.log(2%2); for remainder
+
+let str1 = "hello"
+let str2 = " Yogi"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("3"+2);
+// console.log(3+"2");
+// console.log("3"+"2");
+// console.log(3+5 + "2");
+// console.log("3"+5 + 2);
+// console.log( (9+3) * 5);
+
+console.log(-true)
+
+let num1 , num2, num3
+ 
+num1 = num2 = num3 = num2+num3
+console.log(num1);
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+// link to study prefix and postfix (increment)
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment 
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01-basics/04-comparision.js
@@ -0,0 +1,23 @@
+// console.log(2<1);
+// console.log(2>1);
+// console.log(2==1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+
+console.log("2" > 5);
+console.log("0 2" > 5);
+
+console.log(null >0);
+console.log(null >=0);
+console.log(null ==0);
+// comparision and equality check are different things
+
+console.log(undefined >0);
+console.log(undefined ==0);
+console.log(undefined <=0);
+//these kind of conversions can confuse you so avoid them as much as possible
+// clean code is must
+
+ 
