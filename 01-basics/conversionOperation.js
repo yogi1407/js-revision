3d151934753a789df4d6e130ab94f47bcb76e922
yogi1407 <73849556+yogi1407@users.noreply.github.com>
2024-12-25 18:03:28 +0000

    conversion changess

---
+++ 01-basics/conversionOperation.js
@@ -0,0 +1,40 @@
+//let score = "333"
+//let score = "333aabbcc"
+let score = "333aabbcc"
+
+
+console.log(typeof score);
+console.log(typeof (score));//both are same
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+// 33 is converted into number itself 33
+// true convertes to 1 , false converts to 0
+//33aabbcc converts to NaN(not a number)
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 = true , 0 = false
+// " " = false
+// "yogi" = true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
+
+
