c2b09edafe831cff1fd7a921ad9ad44038222d4f
yogi1407 <73849556+yogi1407@users.noreply.github.com>
2025-01-04 16:50:32 +0000

    math

---
+++ 01-basics/06-mathematics.js
@@ -0,0 +1,40 @@
+const Score = 100
+// console.log(Score);
+
+const balance = new Number(100)
+
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));
+// console.log(typeof balance);
+
+const otherNumber = 13.4924765874758344875639475
+
+// console.log(otherNumber.toPrecision(20));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-in'));
+
+
+
+// ******************************************** MATH ********************************************
+
+// console.log(Math)
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.4));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.9));
+// console.log(Math.sqrt(121));
+// console.log(Math.min(5 , 4 , 5 , 1 ,7));
+// console.log(Math.max(5 , 4 , 5 , 1 ,7));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1 );
+console.log(Math.floor(Math.random()*10) + 1 );
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min);
+ 
+
+
