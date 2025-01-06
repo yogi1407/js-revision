0dded9073c2538db34b2e8c119e43e80e2348a3f
yogi1407 <73849556+yogi1407@users.noreply.github.com>
2025-01-06 21:28:24 +0000

    array changes

--- 02-basics/01-array.js
+++ 02-basics/01-array.js
@@ -1,17 +1,42 @@
-const myArr = [0,1,2,3,4,5,6]
+const myArr = [0,1,2,3,4,]
 const myHeros = ["ironMan", "hulk","spiderMan",'captainAmerica']
 const myArr2 = [1,2,3,4,5]
-console.log(myArr[2]);
-console.log(myHeros[3]);
+// console.log(myArr[2]);
+// console.log(myHeros[3]);
 
 ///////// Array Method ///////////
-myArr.push('Yogi')
+// myArr.push('Yogi')
 myArr.push('Kartik')
 myArr.pop()
 myArr.unshift('one')//most unused because it adds the value in the start of the array so
 //  that we have to change all the element index of the array
 myArr.shift()
-console.log(myArr);
-console.log(myArr.includes(5));
-console.log(myArr.indexOf(3));
+// console.log(myArr);
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()//. converts array into string form
+
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// slice and splice
+
+console.log("A ",myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1)
+console.log("B ",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C ",myArr);
+console.log(myn2);
+
+
+
+
+
+
 
